4639ac21bed78847edf951adb02d4f6a6628d68d
Gustavo Roberto <guroberto.dev@gmail.com>
2025-05-18 20:17:03 -0300

    .

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,13 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   publicDir: "./static",
   base: "./",
+  server: {
+    port: 5173,
+    host: true,
+  },
 });
