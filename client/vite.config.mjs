0af9e163efb37af8962d791e0ccdc1958b4a253b
Gustavo Roberto <guroberto.dev@gmail.com>
2025-05-18 20:34:48 -0300

    .

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -1,18 +1,13 @@
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   publicDir: "./static",
   base: "./",
-  server: {
-    port: process.env.PORT || 5173,
-    host: true,
-  },
   preview: {
     port: process.env.PORT || 8080,
     host: true,
-    allowedHosts: ['client-institutocriativo.up.railway.app'],
+    allowedHosts: "all",
   },
 });
