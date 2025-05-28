e3f6e698b1def14514770886e381852ed58b6571
GSPrograms <Ghenrique.s.santosf@gmail.com>
2025-05-28 10:51:24 -0300

    modificação banco sql

--- banco_instituto.sql
+++ banco_instituto.sql
@@ -1,5 +1,7 @@
+ 
 CREATE DATABASE banco_instituto;
 USE banco_instituto;
+
 
 /*FUNÇÃO PARA MOSTRAR TODAS AS TABELAS*/
 SHOW TABLES;
@@ -22,16 +24,48 @@ DESCRIBE usuario;
 
 /*CRIAÇÃO DA TABELA ADMINSTRADOR*/
 CREATE TABLE adm (
-	id_adm INT PRIMARY KEY AUTO_INCREMENT,
+    id_adm INT PRIMARY KEY AUTO_INCREMENT,
     nome_adm VARCHAR(255) NOT NULL,
+    email_adm varchar(100) NOT NULL,
     sobrenome_adm VARCHAR(255) NOT NULL,
     cpf_adm VARCHAR(14) UNIQUE NOT NULL,
     atuacao_adm VARCHAR(50) NOT NULL,
     cargo_adm VARCHAR(100) NOT NULL,
     nome_empresa VARCHAR(255) NOT NULL,
     cnpj VARCHAR(14) UNIQUE NOT NULL,
+	senha varchar(25) NOT NULL,
     cep_empresa VARCHAR(8) UNIQUE NOT NULL
 );
+
+select * from adm;
+
+
+
+INSERT INTO adm (
+    nome_adm,
+    sobrenome_adm,
+    cpf_adm,
+    atuacao_adm,
+    cargo_adm,
+    nome_empresa,
+    cnpj,
+    cep_empresa
+) VALUES (
+    'Lucas',
+    'Silva',
+    '123.456.789-00',
+    'Organização de Eventos',
+    'Coordenador de Projetos',
+    'Instituto Criativo',
+    '12345678000199',
+    '01234000'
+);
+
+
+
+
+
+
 /*MOSTRA DADOS DA TABELA ADMINISTRADOR*/
 SELECT * FROM adm;
 /*MOSTRAR A ESTRUTURA DA TABELA ADMINISTRADOR*/
@@ -42,14 +76,48 @@ DESCRIBE adm;
 CREATE TABLE eventos (
     id_evento INT PRIMARY KEY AUTO_INCREMENT,
     id_adm INT UNIQUE,
+	tipo varchar(10) NOT NULL,
     nome_evento VARCHAR(255) NOT NULL,
-    preco VARCHAR(100) NOT NULL,
+    preco DECIMAL NOT NULL,
     data_hora DATETIME NOT NULL,
     endereco VARCHAR(255) NOT NULL,
     descricao TEXT,
     organizadores VARCHAR(255),
     FOREIGN KEY (id_adm) REFERENCES adm(id_adm) ON DELETE SET NULL
 );
+alter table eventos add column qtd_ingressoes int(32) NOT NULL default 20;
+alter table eventos add column avaliacao int(5) NOT NULL default 3.5;
+
+
+
+INSERT INTO eventos (
+    id_adm,
+    tipo,
+    nome_evento,
+    preco,
+    data_hora,
+    endereco,
+    descricao,
+    organizadores
+) VALUES (
+    6,
+    'Doação',
+    'Evento DoaçãoTerapia2',
+    50.00,
+    '2025-06-15 19:30:00',
+    'Av. Paulista, 1000 - São Paulo',
+    'Uma palestra sobre técnicas de atenção plena e seu impacto no bem-estar emocional.',
+    'Instituto Criativo'
+);
+
+
+
+update eventos set qtd_ingressoes = 27 where id_evento = '3';
+
+
+
+
+
 /*MOSTRA DADOS DA TABELA EVENTOS*/
 SELECT * FROM eventos;
 /*MOSTRAR A ESTRUTURA DA TABELA EVENTOS*/
